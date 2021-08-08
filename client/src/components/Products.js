d4c643e3220d671bfd94853c41e705bfd36f6098
Tanish Vemu <www.vtani74@gmail.com>
2021-08-09 02:04:26 +0530

    ratings

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
+import Ratings from "../components/Ratings";
 
 const Products = ({ product }) => {
   return (
     <>
-      <Card className='my-3 p-3 rounded'>
+      <Card className='products my-3 p-3 rounded'>
         <a href={`/products/${product._id}`}>
           <Card.Img src={product.image} variant='top' />
         </a>
 
         <Card.Body>
           <a href={`/products/${product._id}`}>
-            <Card.Title as='div'>
+            <Card.Title as='div' className='product-name'>
               <strong>{product.name}</strong>
             </Card.Title>
           </a>
 
-          <Card.Title as='div'>
-            <div className='my-3'>
-              {product.rating} from {product.numReviews} reviews
-            </div>
-          </Card.Title>
-
-          <Card.Title as='h4'>&#x20b9;{product.price}</Card.Title>
+          <div className='d-flex justify-content-between align-items-center'>
+            <Card.Title as='h4'>&#x20b9;{product.price}</Card.Title>
+            <Card.Text as='div'>
+              <Ratings value={product.rating} text={product.numReviews} />
+            </Card.Text>
+          </div>
         </Card.Body>
       </Card>
     </>
